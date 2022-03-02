89c2c4a936f4f61a9df26d997acefaa041956085
Francisco Conelli <fconelli@gmail.com>
2022-03-02 10:15:25 -0300

    first commit

---
+++ RS-MVVM/Screens/Employees/Model/EmployeesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmployeesViewModel.swift
+//  RS-MVVM
+//
+//  Created by Francisco Conelli on 02/03/2022.
+//
+
+import Foundation
---
+++ RS-MVVM/Screens/Employees/View/EmployeesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EmployeesViewController.swift
+//  RS-MVVM
+//
+//  Created by Francisco Conelli on 02/03/2022.
+//
+
+import Foundation
