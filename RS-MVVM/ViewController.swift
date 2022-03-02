e9bb372b62f7b632c28b38d7755a9bb8122937d8
Francisco Conelli <fconelli@gmail.com>
2022-03-02 09:29:38 -0300

    Initial Commit

---
+++ RS-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RS-MVVM
+//
+//  Created by Francisco Conelli on 02/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
