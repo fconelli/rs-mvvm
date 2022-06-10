717691361698d95aff467765ddf77562d3449139
Francisco Conelli <fconelli@gmail.com>
2022-06-10 17:43:41 -0300

    Remove unnecessary methods

--- RS-MVVM/Screens/Employees/View/EmployeeTableViewCell.swift
+++ RS-MVVM/Screens/Employees/View/EmployeeTableViewCell.swift
@@ -13,17 +13,6 @@ class EmployeeTableViewCell: UITableViewCell {
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var salaryLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     func configure(with presentable: EmployeePresentable) {
         nameLabel.text = presentable.name
         ageLabel.text = presentable.age
